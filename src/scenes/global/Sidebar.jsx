e38bf98804a779c5a292fb7db016d485d897eec0
veronica17-web <veronicamiryala001@gmail.com>
2023-09-27 16:32:54 +0530

    first commit

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,287 @@
+import { useState } from 'react';
+import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
+import { Link } from 'react-router-dom';
+import 'react-pro-sidebar/dist/css/styles.css';
+import { tokens } from '../../theme';
+// import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+//import PhoneCallbackIcon from '@mui/icons-material/PhoneCallback';
+import PriceChangeIcon from '@mui/icons-material/PriceChange';
+import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
+import CarCrashIcon from '@mui/icons-material/CarCrash';
+import CarRepairIcon from '@mui/icons-material/CarRepair';
+// import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+import LogoutIcon from '@mui/icons-material/Logout';
+import CorporateFareIcon from '@mui/icons-material/CorporateFare';
+import LocalAtmIcon from '@mui/icons-material/LocalAtm';
+import SportsMotorsportsIcon from '@mui/icons-material/SportsMotorsports';
+// import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+// import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+// import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+// import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+// import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+// import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+// import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+// import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{
+        color: colors.grey[100],
+      }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </MenuItem>
+  );
+};
+
+const Sidebar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  const [selected, setSelected] = useState('Dashboard');
+
+  return (
+    <Box
+      sx={{
+        '& .pro-sidebar-inner': {
+          background: `${colors.blueAccent[700]} !important`,
+        },
+        '& .pro-icon-wrapper': {
+          backgroundColor: 'transparent !important',
+        },
+        '& .pro-inner-item': {
+          padding: '5px 35px 5px 20px !important',
+        },
+        '& .pro-inner-item:hover': {
+          color: '#db4f4a !important',
+        },
+        '& .pro-menu-item.active': {
+          color: '#db4f4a !important',
+        },
+      }}
+    >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape='square'>
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: '10px 0 20px 0',
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display='flex'
+                justifyContent='space-between'
+                alignItems='center'
+                ml='15px'
+              >
+                <Typography variant='h3' color={colors.redAccent[100]}>
+                  {/* Saboo RKS */}
+                  <img
+                    alt='profile-user'
+                    width='100px'
+                    height='100px'
+                    src={`https://saboomaruti.in/static/media/whitelogo.5a5baebbd708786e1e5d.webp`}
+                    // style={{ cursor: 'pointer', borderRadius: '100%' }}
+                  />
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+
+          {!isCollapsed && (
+            <Box mb='25px'>
+              <Box display='flex' justifyContent='center' alignItems='center'>
+                {/* <img
+                  alt='profile-user'
+                  width='100px'
+                  height='100px'
+                  src={`../../assets/sabooautozone.webp`}
+                  // style={{ cursor: 'pointer', borderRadius: '100%' }}
+                /> */}
+              </Box>
+              <Box textAlign='center'>
+                <Typography
+                  variant='h2'
+                  color={colors.grey[100]}
+                  fontWeight='bold'
+                  sx={{ m: '10px 0 0 0' }}
+                >
+                 Arena
+                </Typography>
+                <Typography variant='h5' color={colors.sabooAutoColors[700]}>
+                  By Saboo RKS Group
+                </Typography>
+              </Box>
+            </Box>
+          )}
+
+          <Box paddingLeft={isCollapsed ? undefined : '10%'}>
+            <Typography
+              variant='h6'
+              color={colors.grey[300]}
+              sx={{ m: '15px 0 5px 20px' }}
+            >
+              Data
+            </Typography>
+            {/* <Item
+              title="Manage Team"
+              to="/team"
+              icon={<PeopleOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            /> */}
+           
+            <Item
+              title='Popup'
+              to='/popup'
+              icon={<DirectionsWalkIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title='On-Road Price'
+              to='/onroadprice'
+              icon={<LocalAtmIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+              <Item
+              title='Book a Service'
+              to='/bookaservice'
+              icon={<CarRepairIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title='Finance'
+              to='/finance'
+              icon={<PriceChangeIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title='Insurance'
+              to='/insurance'
+              icon={<CarCrashIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          
+            <Item
+              title='Driving School'
+              to='/drvingschool'
+              icon={<SportsMotorsportsIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title='Corporates'
+              to='/corporates'
+              icon={<CorporateFareIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+             <Item
+              title='AllData'
+              to='/alldata'
+              icon={<LeaderboardIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title='Log Out'
+              to='/logout'
+              icon={<LogoutIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            {/* <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Pages
+            </Typography>
+            <Item
+              title="Profile Form"
+              to="/form"
+              icon={<PersonOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Calendar"
+              to="/calendar"
+              icon={<CalendarTodayOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="FAQ Page"
+              to="/faq"
+              icon={<HelpOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Charts
+            </Typography>
+            <Item
+              title="Bar Chart"
+              to="/bar"
+              icon={<BarChartOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Pie Chart"
+              to="/pie"
+              icon={<PieChartOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Line Chart"
+              to="/line"
+              icon={<TimelineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Geography Chart"
+              to="/geography"
+              icon={<MapOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            /> */}
+          </Box>
+        </Menu>
+      </ProSidebar>
+    </Box>
+  );
+};
+
+export default Sidebar;
---
+++ src/scenes/insurance/index.jsx
@@ -0,0 +1,526 @@
+import { Box, Button } from "@mui/material";
+// import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+
+import LooksOneIcon from "@mui/icons-material/LooksOne";
+import Header from "../../components/Header";
+import { useTheme } from "@mui/material";
+import { useEffect, useState } from "react";
+import axios from "axios";
+//import date range picker files
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
+import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarDensitySelector,
+} from "@mui/x-data-grid";
+import { IconButton } from "@mui/material";
+import DownloadIcon from "@mui/icons-material/Download";
+const Insurance = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState([]);
+  const [col, setCol] = useState([]);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        const res = await axios.get(
+          "https://arena-backend-zj42.onrender.com/getIsurance"
+        );
+        setCol([
+          { field: "id", headerName: "ID", flex: 0.5 },
+          {
+            field: "Last_Name",
+            headerName: "Name",
+            flex: 1,
+          },
+          {
+            field: "Phone",
+            headerName: "Phone Number",
+            flex: 1,
+            cellClassName: "phone-column--cell",
+          },
+          {
+            field: "Email",
+            headerName: "Email",
+            flex: 1,
+          },
+
+          {
+            field: "date",
+            headerName: "Date",
+            flex: 1,
+          },
+          {
+            field: "time",
+            headerName: "Time",
+            flex: 1,
+          },
+        ]);
+        setData(res.data.data);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  let newData = data.map((item, index) => {
+    return { ...item, id: index + 1 };
+  });
+
+  //date range unique function
+
+  async function fetchUniqueValues(startDate, endDate) {
+    try {
+      setLoading(true);
+      const formattedStartDate = new Date(startDate);
+      formattedStartDate.setDate(formattedStartDate.getDate() + 1);
+      const formattedStartDateString = formattedStartDate
+        .toISOString()
+        .slice(0, 10);
+
+      const formattedEndDate = new Date(endDate);
+      formattedEndDate.setDate(formattedEndDate.getDate() + 1);
+      const formattedEndDateString = formattedEndDate
+        .toISOString()
+        .slice(0, 10);
+
+      const res = await axios.post(
+        "https://arena-backend-zj42.onrender.com/insuranceRange",
+        {
+          startDate: formattedStartDateString,
+          endDate: formattedEndDateString,
+        }
+      );
+      setCol([
+        { field: "id", headerName: "ID", flex: 0.5 },
+        {
+          field: "Last_Name",
+          headerName: "Name",
+          flex: 1,
+        },
+        {
+          field: "Phone",
+          headerName: "Phone Number",
+          flex: 1,
+          cellClassName: "phone-column--cell",
+        },
+        {
+          field: "Email",
+          headerName: "Email",
+          flex: 1,
+        },
+
+        {
+          field: "date",
+          headerName: "Date",
+          flex: 1,
+        },
+        {
+          field: "time",
+          headerName: "Time",
+          flex: 1,
+        },
+      ]);
+      setData(res.data.data);
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (startDate && endDate) {
+      fetchUniqueValues(startDate, endDate);
+    }
+  }, [startDate, endDate]);
+
+  const handleReset = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get(
+        "https://arena-backend-zj42.onrender.com/getIsurance"
+      );
+      setCol([
+        { field: "id", headerName: "ID", flex: 0.5 },
+        {
+          field: "Last_Name",
+          headerName: "Name",
+          flex: 1,
+          cellClassName: "name-column--cell",
+        },
+        {
+          field: "Phone",
+          headerName: "Phone Number",
+          flex: 1,
+          cellClassName: "phone-column--cell",
+        },
+        {
+          field: "Email",
+          headerName: "Email",
+          flex: 1,
+        },
+
+        {
+          field: "date",
+          headerName: "Date",
+          flex: 1,
+        },
+        {
+          field: "time",
+          headerName: "Time",
+          flex: 1,
+        },
+      ]);
+      setData(res.data.data);
+
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+      setLoading(false);
+    }
+  };
+
+  const handleDup = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get(
+        "https://arena-backend-zj42.onrender.com/duplicateInsurance"
+      );
+
+      // Process the response data to create rows with phoneNumber, model, and count
+      const processedData = [];
+      let idCounter = 1;
+
+      res.data.data.forEach((item) => {
+        processedData.push({
+          id: idCounter++,
+          phoneNumber: item.number,
+
+          count: item.count,
+          date: item.date, // Adding the date field
+        });
+      });
+
+      setCol([
+        { field: "id", headerName: "ID", flex: 0.5 },
+        {
+          field: "phoneNumber",
+          headerName: "Phone Number",
+          flex: 1,
+          cellClassName: "phone-column--cell",
+        },
+
+        { field: "count", headerName: "Count", flex: 1 },
+        { field: "date", headerName: "Date", flex: 1 }, // Adding the date column
+      ]);
+
+      setData(processedData);
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+      setLoading(false);
+    }
+  };
+  const uniqueEntries = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get(
+        `https://arena-backend-zj42.onrender.com/insuranceUniqueEntries`
+      );
+      setCol([
+        { field: "id", headerName: "ID", flex: 0.5 },
+        {
+          field: "Last_Name",
+          headerName: "Name",
+          flex: 1,
+        },
+        {
+          field: "Phone",
+          headerName: "Phone Number",
+          flex: 1,
+          cellClassName: "phone-column--cell",
+        },
+        {
+          field: "Email",
+          headerName: "Email",
+          flex: 1,
+        },
+
+        {
+          field: "date",
+          headerName: "Date",
+          flex: 1,
+        },
+        {
+          field: "time",
+          headerName: "Time",
+          flex: 1,
+        },
+      ]);
+      setData(res.data.data);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  const handleDownloadCSV = () => {
+    const csvData = [];
+    const headers = col.map((column) => column.headerName);
+    csvData.push(headers);
+
+    newData.forEach((item) => {
+      const row = col.map((column) => item[column.field]);
+      csvData.push(row);
+    });
+
+    const csvContent = csvData.map((row) => row.join(",")).join("\n");
+
+    const blob = new Blob([csvContent], { type: "text/csv" });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.style.display = "none";
+    a.href = url;
+    a.download = "Insurance(Arena).csv";
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+  };
+
+  // Custom toolbar with the download button
+  
+const CustomToolbar = () => {
+    return (
+      <GridToolbarContainer>
+        <GridToolbarColumnsButton />
+        <GridToolbarFilterButton />
+        <GridToolbarDensitySelector />
+        <IconButton
+          color="primary"
+          onClick={handleDownloadCSV}
+          sx={{
+            marginLeft: "10px",
+            backgroundColor: "white",
+            fontSize: "14px",
+            padding: "5px",
+            minWidth: "auto",
+            height: "25px",
+            color:"#3e4396"
+          }}
+        >
+          <DownloadIcon />
+        </IconButton>
+      </GridToolbarContainer>
+    );
+  };
+  return (
+    <Box m="20px">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        {" "}
+        <Header
+          title="Insurance"
+          subtitle="List of Insurance data for Future Reference"
+        />
+        
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ marginRight: "10px" }}>
+            {" "}
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer
+                components={["DateRangePicker"]}
+                sx={{ padding: "6px", backgroundColor: "transparent" }}
+              >
+                <DateRangePicker
+                  localeText={{
+                    start: (
+                      <span style={{ fontSize: "16px", padding: "2px" }}>
+                        Start Date
+                      </span>
+                    ),
+                    end: (
+                      <span style={{ fontSize: "16px", padding: "2px" }}>
+                        End Date
+                      </span>
+                    ),
+                  }}
+                  start={startDate}
+                  end={endDate}
+                  onChange={(newValue) => {
+                    setStartDate(newValue[0]);
+                    setEndDate(newValue[1]);
+                  }}
+                />
+              </DemoContainer>
+            </LocalizationProvider>
+          </div>
+
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ backgroundColor: "#3e4396", mr: 2, color: "white",  '&:hover': {
+              backgroundColor: "red",
+            }, }}
+            onClick={handleDup}
+          >
+            Duplicates
+          </Button>
+
+          {/* <input
+                  type='date'
+                  required
+                  sx={{ mr: 2, backgroundColor: '#940004' }}
+                  value={inputValue}
+                  onChange={(e) => {
+                    const newInputValue = e.target.value;
+                    console.log('New input value:', newInputValue);
+                    setInputValue(newInputValue);
+                    handleRemoveDuplicates(newInputValue);
+                  }}
+                  style={{
+                    backgroundColor: '#940004',
+                    color: 'white',
+                    borderRadius: '6px',
+                    border: 'none',
+                    padding: '6px',
+                    margin: '15px', // Add margin to separate input and button
+                    flex: 1,
+                    // Allow the input to grow to fill available space
+                  }}
+                /> */}
+
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ mr: 2, backgroundColor: "#3e4396", color: "white" ,  '&:hover': {
+              backgroundColor: "red",
+            }, }}
+            onClick={uniqueEntries}
+          >
+            {" "}
+            <LooksOneIcon />
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ backgroundColor: "#3e4396", color: "white" ,  '&:hover': {
+              backgroundColor: "red",
+            }, }}
+            onClick={handleReset}
+          >
+            Reset
+          </Button>
+          {/* <Button
+                  variant='contained'
+                  color='primary'
+                  sx={{ ml: 2, backgroundColor: '#940004' }}
+                  onClick={handleRemoveDuplicates}
+                >
+                  Unique
+                </Button>
+                <input
+                  type='date'
+                  required
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                  style={{
+                    marginLeft: '16px',
+                    backgroundColor: '#940004',
+                    color: 'white',
+                    borderRadius: '8px',
+                    border: 'none',
+                    padding: '8px',
+                  }}
+                /> */}
+        </div>
+      </div>
+
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+            backgroundColor: "white",
+            // border: "1px solid #ccc", // Add a border to the table
+          },
+          "& .MuiDataGrid-cell": {
+            //borderBottom: "none",
+            backgroundColor: "white",
+            borderBottom: "1px solid #ccc", // Add a border to table cells
+          },
+          "& .phone-column--cell": {
+            color: colors.sabooAutoColors[500],
+            backgroundColor: "white",
+          },
+          "& .MuiDataGrid-columnHeader": {
+            color: "white",
+            backgroundColor: colors.blueAccent[700], // Optional background color for headers
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.sabooAutoColors[400],
+          },
+          // "& .MuiDataGrid-footerContainer": {
+          //   borderTop: "none",
+          //   backgroundColor: colors.blueAccent[700],
+          //   "& .MuiTypography-root": {
+          //     color: "white", // Change the footer text color to white
+          //   },
+          // },
+          "& .MuiCheckbox-root": {
+            color: `${colors.blueAccent[700]} !important`,
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.blueAccent[700]} !important`,
+          },
+          '& .MuiDataGrid-sortIcon': {
+            color:'white',
+          },
+          '& .css-196n7va-MuiSvgIcon-root': {
+            color:'white',
+          },
+        }}
+      >
+        {loading ? (
+          <div style={{ fontSize: '14px' }}>Processing, please wait...</div>
+        ) : error ? (
+          "Error ~ Something went wrong :)"
+        ) : (
+          <DataGrid
+            rows={newData}
+            columns={col}
+            components={{ Toolbar: CustomToolbar }}
+            sx={{
+              backgroundColor: "white", // Set the background color to white
+            }}
+          />
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default Insurance;
