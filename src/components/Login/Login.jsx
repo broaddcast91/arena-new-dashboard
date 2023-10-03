621056485f6af4583648757544d951b5e4c3b0bf
veronica17-web <veronicamiryala001@gmail.com>
2023-10-03 11:03:27 +0530

    added authentication to routes

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,70 +1,175 @@
-import { useState } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import Topbar from './scenes/global/Topbar';
-import Sidebar from './scenes/global/Sidebar';
-import Popup from './scenes/popup';
-import Corporates from './scenes/Corporates';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { ColorModeContext, useMode } from './theme';
-import OnRoadPrice from './scenes/onroadprice';
-import Finance from './scenes/finance';
-import Insurance from './scenes/insurance';
-import BookAService from './scenes/bookaservice';
-import DrvingSchool from './scenes/DrvingSchool/DrvingSchool';
-import Logout from './components/Logout/Logout';
-import AllData from './scenes/AllData';
-import Login from './components/Login/Login';
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
-function App() {
-  const [theme, colorMode] = useMode();
-  const [isSidebar, setIsSidebar] = useState(true);
-  const location = useLocation();
+const defaultTheme = createTheme();
 
-  // Check if the current location is the login page or the root path
-  const isLoginPage = location.pathname === '/login' || location.pathname === '/';
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-  // Check if the user is authenticated (e.g., by checking a token in localStorage)
-  const isAuthenticated = localStorage.getItem('token'); // Replace with your authentication logic
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-  // Conditionally render the Sidebar based on authentication status
-  const shouldRenderSidebar = isAuthenticated && !isLoginPage;
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-  // Conditionally render routes based on authentication status
-  const routes = isAuthenticated ? (
-    <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/popup' element={<Popup />} />
-      <Route path='/onroadprice' element={<OnRoadPrice />} />
-      <Route path='/corporates' element={<Corporates />} />
-      <Route path='/finance' element={<Finance />} />
-      <Route path='/insurance' element={<Insurance />} />
-      <Route path='/alldata' element={<AllData />} />
-      <Route path='/bookaservice' element={<BookAService />} />
-      <Route path='/drvingschool' element={<DrvingSchool />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/logout' element={<Logout />} />
-    </Routes>
-  ) : (
-    <Routes>
-      <Route path='/' element={<Login />} />
-    </Routes>
-  );
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "https://arena-backend-zj42.onrender.com/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+
+      if (response.ok) {
+        const { token } = await response.json();
+        localStorage.setItem("authToken", token);
+        window.location.href = "https://arena-dashboard.netlify.app/popup";
+      } else {
+        setError("Invalid email or password");
+      }
+    } catch (error) {
+      console.error("Error logging in:", error);
+    }
+  };
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className='app'>
-          {/* Conditionally render the Sidebar */}
-          {shouldRenderSidebar ? <Sidebar isSidebar={isSidebar} /> : null}
-          <main className='content'>
-            <Topbar setIsSidebar={setIsSidebar} />
-            {routes}
-          </main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  );
-}
+    <ThemeProvider theme={defaultTheme}>
+      <Box sx={{ flexGrow: 1 }}>
+       
+      </Box>
 
-export default App;
+      <Grid container component="main" sx={{ height: "calc(100vh - 64px)" }}>
+        <CssBaseline />
+        <Grid
+  item
+  xs={false}
+  sm={6}
+  md={7}
+  sx={{
+    backgroundImage:
+      "url(https://miro.medium.com/v2/resize:fit:1358/1*9m-WDdL_ji01bGbjEnutEw.gif)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor: "light",
+    backgroundPosition: "center",
+    height: "500px",
+    width: "400px",
+    marginTop:"80px",
+    backgroundSize: "contain", // Adjust this property for the size you want
+  }}
+/>
+        <Grid item xs={12} sm={8} md={5}>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              marginRight:"100px"
+            }}
+          >
+             <img
+          src="https://www.saboogroups.com/assets/images/black-logo.png"
+          alt="Logo"
+          height="400"
+          width="150"
+          style={{ marginRight: "16px", marginTop: "-20px" }}
+        />
+            <Avatar sx={{ m: 1, backgroundColor: "#3e4396" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5" sx={{ fontWeight: "bold" }}>
+              Sign in
+            </Typography>
+
+            {error && <p className="text-danger">{error}</p>}
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={email}
+                onChange={handleEmailChange}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type={showPassword ? "text" : "password"}
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={handlePasswordChange}
+                InputProps={{
+                  endAdornment: (
+                    <IconButton
+                      onClick={toggleShowPassword}
+                      aria-label={showPassword ? "Hide Password" : "Show Password"}
+                    >
+                      {showPassword ? <VisibilityIcon />   : <VisibilityOffIcon />}
+                    </IconButton>
+                  ),
+                }}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2, backgroundColor: "#3e4396", borderRadius:"10px" }}
+              >
+                Login In
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+};
+
+export default Login;
